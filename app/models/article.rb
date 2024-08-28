d1fb1117cb1c0257de7c1314ebb7cba60f681583
takeshi <goozooloo123@gmail.com>
2024-08-28 15:49:44 +0900

    ユーザと記事の関連付け, new&createアクション, edit&updateアクション, destroyアクション に対応

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,6 +7,11 @@
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
     validates :title, presence: true
@@ -19,9 +24,12 @@ class Article < ApplicationRecord
 
     validate :validate_title_and_content_length
 
+    belongs_to :user
+
     def display_created_at
         I18n.l(self.created_at, format: :default)
     end
+    
 
     private
     def validate_title_and_content_length
