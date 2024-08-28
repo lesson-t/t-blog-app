19f87f0f113b252df3a8347b1c837f879720eb11
takeshi <goozooloo123@gmail.com>
2024-08-29 07:11:54 +0900

    Commentモデルの作成, ルーティングの設定,new&createアクションの設定, 投稿画面の作成, 動作確認

--- app/models/article.rb
+++ app/models/article.rb
@@ -24,12 +24,13 @@ class Article < ApplicationRecord
 
     validate :validate_title_and_content_length
 
+    has_many :comments, dependent: :destroy
     belongs_to :user
 
     def display_created_at
         I18n.l(self.created_at, format: :default)
     end
-    
+
     def author_name
         user.display_name
     end
