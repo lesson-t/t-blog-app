c2b7a39855e19f92421d135bcef8ff08255fd547
takeshi <goozooloo123@gmail.com>
2024-09-15 21:23:15 +0900

    js ファイルの分割, webpack-dev-server の動作不良の修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,9 @@
 const { environment } = require('@rails/webpacker')
 
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
+
 module.exports = environment
